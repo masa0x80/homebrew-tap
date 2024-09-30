19faaf25044ec5c899070d02544f5b30e247777c
KIMURA Masayuki <masa0x80@gmail.com>
2024-09-30 23:45:42 +0900

    ✏️ fix wrong filenames

--- Casks/font-explex-nf.rb
+++ Casks/font-explex-nf.rb
@@ -6,23 +6,13 @@ cask "font-explex-nf" do
   name "Explex NF"
   homepage "https://github.com/yuru7/Explex"
 
-  font "Explex_v#{version}/Explex/Explex-Bold.ttf"
-  font "Explex_v#{version}/Explex/Explex-BoldItalic.ttf"
-  font "Explex_v#{version}/Explex/Explex-Italic.ttf"
-  font "Explex_v#{version}/Explex/Explex-Regular.ttf"
+  font "Explex_NF_v#{version}/Explex35Console_NF/Explex35ConsoleNF-Bold.ttf"
+  font "Explex_NF_v#{version}/Explex35Console_NF/Explex35ConsoleNF-BoldItalic.ttf"
+  font "Explex_NF_v#{version}/Explex35Console_NF/Explex35ConsoleNF-Italic.ttf"
+  font "Explex_NF_v#{version}/Explex35Console_NF/Explex35ConsoleNF-Regular.ttf"
 
-  font "Explex_v#{version}/Explex35/Explex35-Bold.ttf"
-  font "Explex_v#{version}/Explex35/Explex35-BoldItalic.ttf"
-  font "Explex_v#{version}/Explex35/Explex35-Italic.ttf"
-  font "Explex_v#{version}/Explex35/Explex35-Regular.ttf"
-
-  font "Explex_v#{version}/Explex35Console/Explex35Console-Bold.ttf"
-  font "Explex_v#{version}/Explex35Console/Explex35Console-BoldItalic.ttf"
-  font "Explex_v#{version}/Explex35Console/Explex35Console-Italic.ttf"
-  font "Explex_v#{version}/Explex35Console/Explex35Console-Regular.ttf"
-
-  font "Explex_v#{version}/ExplexConsole/ExplexConsole-Bold.ttf"
-  font "Explex_v#{version}/ExplexConsole/ExplexConsole-BoldItalic.ttf"
-  font "Explex_v#{version}/ExplexConsole/ExplexConsole-Italic.ttf"
-  font "Explex_v#{version}/ExplexConsole/ExplexConsole-Regular.ttf"
+  font "Explex_NF_v#{version}/ExplexConsole_NF/ExplexConsoleNF-Bold.ttf"
+  font "Explex_NF_v#{version}/ExplexConsole_NF/ExplexConsoleNF-BoldItalic.ttf"
+  font "Explex_NF_v#{version}/ExplexConsole_NF/ExplexConsoleNF-Italic.ttf"
+  font "Explex_NF_v#{version}/ExplexConsole_NF/ExplexConsoleNF-Regular.ttf"
 end
